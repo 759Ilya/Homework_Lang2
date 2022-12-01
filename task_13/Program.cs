54f3d4773fe72a0df2d8a07bd04f8a2d2927c377
Ilya <doggi.v@mail.ru>
2022-12-01 15:58:45 +0600

    task_15 complete

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -9,9 +9,11 @@ Console.WriteLine("Задача 13 ");
 Console.Write("Введи число: ");
 int anyNumber = Convert.ToInt32(Console.ReadLine());
 string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2){
+if (anyNumberText.Length > 2)
+{
   Console.WriteLine("третья цифра -> " + anyNumberText[2]);
 }
-else {
+else 
+{
   Console.WriteLine("-> третьей цифры нет");
 }
